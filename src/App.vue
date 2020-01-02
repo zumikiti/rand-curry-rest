f958c33b9393e448ff84f86fe816963c220f23c2
akihiro_note <higashiizumi@qnote.co.jp>
2020-01-02 09:18:14 +0900

    [add] ランダムカレーアプリ作成

---
+++ src/App.vue
@@ -0,0 +1,108 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="amber darken-4"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content v-if="rest">
+      <rest-item
+        :key="rest.id"
+        :rest="rest"
+      />
+      <div align="center">
+        <v-btn
+          align="center"
+          color="amber darken-2"
+          dark
+          large
+          @click="reFetchRests"
+        >
+          次のカレー屋さん
+        </v-btn>
+      </div>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  import RestItem from './components/Item'
+  import axios from 'axios'
+
+  export default {
+    name: 'App',
+
+    components: {
+      RestItem,
+    },
+
+    props: {
+      baseApi: {
+        type: String,
+        default: `https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=${process.env.VUE_APP_GNAVI_ACCESS_KEY}&freeword=カレー`,
+      },
+    },
+
+    data: () => ({
+      rests: {},
+      total_rest_count: 0,
+    }),
+
+    computed: {
+      rest () {
+        return this.rests[Math.floor(Math.random() * 10)]
+      },
+    },
+
+    mounted () {
+      this.fetchRests()
+    },
+
+    methods: {
+      async fetchRests () {
+        const res = await axios
+          .get(this.baseApi)
+        this.total_rest_count = res.data.total_hit_count
+        this.rests = res.data.rest
+      },
+
+      async reFetchRests () {
+        const offset = Math.floor(Math.random() * 1000)
+        const res = await axios
+          .get(`${this.baseApi}&offset=${offset}`)
+
+        this.rests = res.data.rest
+      },
+    },
+  }
+</script>
---
+++ src/components/Item.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-card
+    :loading="loading"
+    class="mx-auto my-12"
+    max-width="374"
+  >
+    <v-img
+      alt="Vuetify Logo"
+      class="shrink mr-2"
+      contain
+      :src="rest.image_url.shop_image1 || '/curry_indian_man.png'"
+      transition="scale-transition"
+      height="250"
+    />
+
+    <v-card-title>{{ rest.name }}</v-card-title>
+
+    <v-card-subtitle>
+      {{ rest.code.prefname }} <br>
+      {{ rest.access.line }}{{ rest.access.station }}から徒歩{{ rest.access.walk }}分
+    </v-card-subtitle>
+
+    <v-card-actions>
+      <v-btn
+        color="deep-purple accent-4"
+        text
+        :href="rest.url"
+      >
+        詳細表示
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: 'RestItem',
+
+    props: {
+      rest: {
+        type: Object,
+        required: true,
+      },
+    },
+
+    data: () => ({
+      loading: false,
+      selection: 1,
+    }),
+
+    methods: {
+      reserve () {
+        this.loading = true
+
+        setTimeout(() => (this.loading = false), 2000)
+      },
+    },
+  }
+</script>
